755c5e7517b144a62605019a9552809fba1c60e0
Payam Mirzaei <105823335+payammirzaei@users.noreply.github.com>
2026-09-10 21:20:33 +0100

    Switch Data Journey to stable EDC topology scene

--- components/journey/NeuralScene.tsx
+++ components/journey/NeuralScene.tsx
@@ -1,3 +1,3 @@
 export { default as SimpleScene } from "./SimpleJourneyScene";
 export type { SceneProps } from "./journey-scene-types";
-export { default } from "./EdcJourneyScene";
+export { default } from "./EdcJourneySceneV2";
